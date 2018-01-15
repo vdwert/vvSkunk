5abd9fee5d620268ea4b252d56fdc13acd53ec3c
Marc van de Wert <marc@marcvandewert.nl>
2018-01-15 19:19:42 +0100

    Updated the version numbers.

--- Skunk.Website/Properties/AssemblyInfo.cs
+++ Skunk.Website/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.0.2")]
-[assembly: AssemblyFileVersion("1.2017.1212.1933")]
+[assembly: AssemblyVersion("1.5.0.4")]
+[assembly: AssemblyFileVersion("1.2018.0115.1919")]
